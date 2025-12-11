095a855df50b499c26f4e3cf5c43f417e5ae7750
hechmi10 <hechmi.jaidane@gmail.com>
2025-12-11 16:23:05 +0100

    Authentication

--- UserApp/views.py
+++ UserApp/views.py
@@ -3,16 +3,23 @@ from django.http import HttpResponse
 from UserApp.models import User
 from django.views.generic import CreateView, FormView
 from django.contrib.auth.views import LoginView
+from django import forms
+
 # Create your views here.
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
 class RegisterView(FormView):
     template_name = 'register.html'
-    model = User
-    fields = ['username', 'email', 'password']
+    form_class = UserForm
     success_url = '/'
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
 class CustomLoginView(LoginView):
     template_name = 'login.html'
     redirect_authenticated_user = True
